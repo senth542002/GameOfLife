9a45ceacbf2782cdef9af4584bc44acba90b839a
senth542002 <senth542002@gmail.com>
2016-08-16 08:07:12 +0530

    second commit

--- GameOfLife/src/main/java/com/java/game/Universe.java
+++ GameOfLife/src/main/java/com/java/game/Universe.java
@@ -14,13 +14,54 @@ public class Universe {
 	public Cell[][] applyRule() {
 		Cell[][] newGrid = new Cell[grid.length][grid.length];
 		for(int i=0; i< grid.length; i++) {
-			for(int j=0; j< grid.length; j++){				
-				newGrid[i][j] = grid[i][j].applyRules(grid, i, j);
+			for(int j=0; j< grid.length; j++){	
+				List<Boolean> neighbouringCellsStatus = identifyNeighbourCells(grid, i, j);
+				newGrid[i][j] = grid[i][j].applyRules(neighbouringCellsStatus, i, j);
 			}
 		}
 		return newGrid;
 	}
 
+	public List<Boolean> identifyNeighbourCells(Cell[][] grid, int i , int j) {
+		List<Boolean> cellNeighboursState = new ArrayList<Boolean>();
+
+		System.out.println("Neighbours for the Cell "+i+","+j);
+		if(j< grid.length -1 ){
+			System.out.println("NothNeighbour Value"+grid[i][j+1].isAlive());
+			cellNeighboursState.add(grid[i][j+1].isAlive());
+		}
+		if(i < grid.length -1){
+			System.out.println("EastNeighbour Value"+grid[i+1][j].isAlive());
+			cellNeighboursState.add(grid[i+1][j].isAlive());
+		}
+		if(i < grid.length-1 && j< grid.length-1 ){
+			
+			System.out.println("NorthEastNeighbour Value"+grid[i+1][j+1].isAlive());
+			cellNeighboursState.add(grid[i+1][j+1].isAlive());
+		}
+		
+		if(j>0){
+			if(i < grid.length-1){						
+				System.out.println("SouthEastNeighbour Value"+grid[i+1][j-1].isAlive());
+				cellNeighboursState.add(grid[i+1][j-1].isAlive());
+			}
+			System.out.println("SouthNeighbour Value"+grid[i][j-1].isAlive());
+			cellNeighboursState.add(grid[i][j-1].isAlive());
+		}
+		if(i>0){
+			System.out.println("WestNeighbour Value"+grid[i-1][j].isAlive());
+			cellNeighboursState.add(grid[i-1][j].isAlive());
+			if(j < grid.length-1) {						
+				System.out.println("NorthWestNeighbour Value"+grid[i-1][j+1].isAlive());
+				cellNeighboursState.add(grid[i-1][j+1].isAlive());
+			}
+		}
+		if(i>0 && j>0){					
+			System.out.println("SouthWestNeighbour Value"+grid[i-1][j-1].isAlive());
+			cellNeighboursState.add(grid[i-1][j-1].isAlive());
+		}
+		return cellNeighboursState;
+}
 	
 	
 }
--- GameOfLife/src/test/java/com/java/game/GameOfLifeAndDeath.java
+++ GameOfLife/src/test/java/com/java/game/GameOfLifeAndDeath.java
@@ -2,17 +2,15 @@ package com.java.game;
 
 import static org.junit.Assert.assertTrue;
 
-import java.util.List;
-
 import org.junit.Test;
 
 public class GameOfLifeAndDeath 
 {
-	public Cell[][] prepareTwoDimentionalUniverseOf(int x,int y) {
+	public Cell[][] prepareTwoDimentionalUniverseOf(int x,int y, boolean aliveOrDead) {
 		Cell[][] grid = new Cell[x][y];
 		for(int i=0; i< grid.length; i++ ){
 			for(int j=0; j<grid.length; j++) {
-				grid[i][j] = new Cell(true);
+				grid[i][j] = new Cell(aliveOrDead);
 			}
 		}
 		return grid;
@@ -20,7 +18,7 @@ public class GameOfLifeAndDeath
 	
     @Test
 	public void SingleCellUniverse() throws Exception {
-    	Cell[][] grid = prepareTwoDimentionalUniverseOf(1,1);
+    	Cell[][] grid = prepareTwoDimentionalUniverseOf(1,1, true);
 		Universe universe = new Universe(grid);
 		Cell[][] gridStatus = universe.applyRule();
 		assertTrue(true==gridStatus[0][0].isAlive());
@@ -29,7 +27,7 @@ public class GameOfLifeAndDeath
     
     @Test
 	public void TwoCellUniverseBoat() throws Exception {
-    	Cell[][] grid = prepareTwoDimentionalUniverseOf(2,2);
+    	Cell[][] grid = prepareTwoDimentionalUniverseOf(2,2, true);
     	Universe universe = new Universe(grid);
 		Cell[][] gridStatus = universe.applyRule();
 		assertTrue(true==gridStatus[0][0].isAlive());
@@ -40,7 +38,7 @@ public class GameOfLifeAndDeath
     
     @Test
 	public void ThreeCellUniverseBlinker() throws Exception {
-    	Cell[][] grid = prepareTwoDimentionalUniverseOf(3,3);
+    	Cell[][] grid = prepareTwoDimentionalUniverseOf(3,3, true);
     	grid[0][0] = new Cell(false);
     	grid[0][1] = new Cell(false);
     	grid[0][2] = new Cell(false);
@@ -62,7 +60,7 @@ public class GameOfLifeAndDeath
     
     @Test
    	public void ThreeCellUniverse() throws Exception {
-       	Cell[][] grid = prepareTwoDimentionalUniverseOf(3,3);
+       	Cell[][] grid = prepareTwoDimentionalUniverseOf(3,3, true);
        	grid[2][2] = new Cell(false);
        	grid[1][1] = new Cell(false);
        	grid[0][0] = new Cell(false);
@@ -78,6 +76,35 @@ public class GameOfLifeAndDeath
    		assertTrue(false==gridStatus[2][0].isAlive());
    		assertTrue(true==gridStatus[2][1].isAlive());
    		assertTrue(false==gridStatus[2][2].isAlive());
+   	}
+    
+    @Test
+   	public void FiveCellUniverse() throws Exception {
+       	Cell[][] grid = prepareTwoDimentionalUniverseOf(5,5, false);
+       	grid[1][1] = new Cell(true);
+       	grid[2][1] = new Cell(true);
+       	grid[3][1] = new Cell(true);
+       	grid[2][2] = new Cell(true);
+       	grid[3][2] = new Cell(true);
+       	grid[4][2] = new Cell(true);
+       	Universe universe = new Universe(grid);
+   		Cell[][] gridStatus = universe.applyRule();
+   		assertTrue(false==gridStatus[1][0].isAlive());
+   		assertTrue(true==gridStatus[2][0].isAlive());
+   		assertTrue(false==gridStatus[3][0].isAlive());
+   		assertTrue(false==gridStatus[4][0].isAlive());
+   		assertTrue(true==gridStatus[1][1].isAlive());
+   		assertTrue(false==gridStatus[2][1].isAlive());
+   		assertTrue(false==gridStatus[3][1].isAlive());
+   		assertTrue(true==gridStatus[4][1].isAlive());
+   		assertTrue(true==gridStatus[1][2].isAlive());
+   		assertTrue(false==gridStatus[2][2].isAlive());
+   		assertTrue(false==gridStatus[3][2].isAlive());
+   		assertTrue(true==gridStatus[4][2].isAlive());
+   		assertTrue(false==gridStatus[1][3].isAlive());
+   		assertTrue(false==gridStatus[2][3].isAlive());
+   		assertTrue(true==gridStatus[3][3].isAlive());
+   		assertTrue(false==gridStatus[4][3].isAlive());
    	}
     
    /* @Test
